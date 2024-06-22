4cd3dfb12ba9e10f8d6a6de649d4c0ba0bc90421
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-06-22 11:51:26 -0400

    Add files via upload
    This is one of my basic python programs, which takes a number from the user and tells them if it is more than 10 or not.

---
+++ Comparison with 10.py
@@ -0,0 +1,7 @@
+x= int(input("Hello, what is your number? "))
+if x<10:
+    print("Your number is less than 10!")
+elif x==10:
+    print("Your number is equal to 10!")
+else :
+    print("Your number is greater than 10!")
