6c9d2592fe5d691bec5c2447e7c9c4b6a304b197
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-07-04 21:11:10 -0400

    Add files via upload
    This python program shuffles all different types of cards: Hearts ,Spades, Cloves, Diamonds.

---
+++ RandomCardtype.py
@@ -0,0 +1,8 @@
+import random
+print("This program will shuffle all the different card types")
+
+cards= ["Spade","Heart","Clove","Diamond"]
+random.shuffle(cards)
+for card in cards:
+    print(cards)
+    
