6cf46a58f95750ad2dbff95dc7a57474c8911829
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-07-08 11:13:53 -0400

    Add files via upload
    This program converts Celsius to Fahrenheit.

---
+++ CelsiusTOFahrenheit.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius}°C is equal to {fahrenheit:.2f}°F.")
+
+celsius_to_fahrenheit()
