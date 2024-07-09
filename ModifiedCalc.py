a9f92f0a28886cd282ed912a48f0751b241ad4a8
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-07-09 10:35:51 -0400

    Add files via upload
    This is a modified version of my calculator, which is structured on a loop.

---
+++ ModifiedCalc.py
@@ -0,0 +1,42 @@
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error! Division by zero."
+x = int(input("What is your first number?"))
+y = int(input("What is your second number?"))
+
+def start():
+    while True:
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Quit")
+        operation= int(input("Which operation would you like to do?"))
+        
+        if operation== (5):
+            print("Bye!")
+            break
+
+
+        elif operation ==(1):
+            print(x+y)
+        elif operation ==(2):
+            print(x-y)
+        elif operation==(3):
+            print(x*y)
+        elif operation==(4):
+            print(x/y)
+        else:
+            print("That is an invalid operation.")
+start()
