6405cc0645089be0df49d044e509369c0e4b7ca0
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-07-14 15:35:48 -0400

    Add files via upload
    This program creates the Olympic rings.

---
+++ olympicrings.java
@@ -0,0 +1,5 @@
+Circle(70, 175, 60, fill=None, border='steelBlue', borderWidth=10)
+Circle(135, 225, 60, fill=None, border='gold', borderWidth=10)
+Circle(200, 175, 60, fill=None, border='black', borderWidth=10)
+Circle(265, 225, 60, fill=None, border='forestGreen', borderWidth=10)
+Circle(330, 175, 60, fill=None, border='crimson', borderWidth=10)
