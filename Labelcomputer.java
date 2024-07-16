43831e03963c3f5ace3f2f289314c65e5eaab615
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-07-16 10:19:02 -0400

    Add files via upload
    This graphical program creates a computer that projects a screen that says "Hello World".

---
+++ Labelcomputer.java
@@ -0,0 +1,8 @@
+Rect(0, 0, 400, 400, border='dimGrey', borderWidth=40)
+
+
+
+Label('System Loading...',140,75,fill='lime',font='arial',size=18)
+Label('Accepting Inputs:',135,100,fill='lime',font='arial',size=18)
+Label('> python hello_world.py',195,140,fill='lime',font='montserrat',size=20)
+Label('Good Evening, Earth',170,200,fill='lime',font='orbitron',size=18)
