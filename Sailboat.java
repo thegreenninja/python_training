7b3aa30ad7e73119d45957e8836b05aceb6cd55c
Adiboy11 <69729309+thegreenninja@users.noreply.github.com>
2024-07-18 12:01:33 -0400

    Add files via upload
    This is a picture of a a sailboat using graphics.

---
+++ Sailboat.java
@@ -0,0 +1,16 @@
+p# background
+Rect(0, 0, 400, 400, fill='wheat')
+
+
+RegularPolygon(210,210,180,7,fill='tan')
+RegularPolygon(200,200,180,7,fill='powderBlue')
+
+
+Circle(270,200,65,fill='orangeRed',opacity=70)
+Circle(270,200,55,fill=gradient('orange','orangeRed'))
+
+Polygon(60,205,190,205,160,240,90,240,fill='dimGray')
+Polygon(125,135,125,205,60,205,fill='forestGreen')
+Polygon(130,145,190,205,130,205,fill='limeGreen')
+
+Polygon(25,240,375,240,280,365,120,365,fill='cadetBlue')
